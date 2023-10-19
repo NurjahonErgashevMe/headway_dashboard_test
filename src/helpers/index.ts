a234b168ed2bda4080e9ea5efc3ceb309e607de0
NurjahonErgashevMe <nurjahonergashevme@gmail.com>
2023-10-19 21:18:23 +0500

    deleted /api/

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
-const url = "https://www.stroy-store-backend.uz/api";
+const url = "https://www.stroy-store-backend.uz";
 export { url  };
 export { default as flatten} from './flutten';
 export { default as mask} from './mask';
